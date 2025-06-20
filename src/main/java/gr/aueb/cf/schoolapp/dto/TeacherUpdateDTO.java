037f8cb88fa995809f72f4d09073e1575f05edb6
D. Chatzimitsos <dim.chat.93@gmail.com>
2025-06-20 15:43:07 +0300

    initial commit

---
+++ src/main/java/gr/aueb/cf/schoolapp/dto/TeacherUpdateDTO.java
@@ -0,0 +1,31 @@
+package gr.aueb.cf.schoolapp.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+//@Data - getter, setter, toString, equals, hashCode
+public class TeacherUpdateDTO {
+
+    @NotNull(message = "Ο κωδικός δεν μπορεί να μην υπάρχει")
+    private Long id;
+
+    @NotNull(message = "Το όνομα δεν μπορεί να μην υπάρχει")
+    @Size(min = 2, max = 255, message = "Το όνομα πρέπει να είναι μεταξύ 2-255 χαρακτήρων.")
+    private String firstname;
+
+    @NotNull(message = "Το επώνυμο δεν μπορεί να μην υπάρχει")
+    @Size(min = 2, max = 255, message = "Το επώνυμο πρέπει να είναι μεταξύ 2-255 χαρακτήρων.")
+    private String lastname;
+
+    @NotNull(message = "Το ΑΦΜ δεν μπορεί να μην υπάρχει")
+    @Size(min = 9, message = "Το ΑΦΜ πρέπει να περιέχει τουλάχιστον 9 ψηφία")
+    private String vat;
+}
